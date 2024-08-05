b91b62b9add979a814239de073e14aaf8ba708c5
Shakir <em.shakir@outlook.com>
2024-08-05 22:26:34 +0530

    [MS050820241026DS] - Added Linked list collection.

---
+++ src/main/java/com/ms/data/structures/linked_list/Node.java
@@ -0,0 +1,12 @@
+package com.ms.data.structures.linked_list;
+
+public class Node<T> {
+
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
