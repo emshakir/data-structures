d7866dfb1264c7b1486ed2ebce9a07a2b7e274da
Shakir <em.shakir@outlook.com>
2024-08-07 20:18:24 +0530

    [MS070820240817DS] - Added doubly linked list and reverse doubly linked list

---
+++ src/main/java/com/ms/data/structures/linked_list/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package com.ms.data.structures.linked_list;
+
+public class ReverseLinkedList<T> {
+
+
+    public Node<T> reverse(Node<T> head) {
+        Node<T> current = head;
+        Node<T> previous = null;
+        Node<T> next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+    public DoublyNode<T> reverseDoubly(DoublyNode<T> head) {
+        DoublyNode<T> current = head;
+        DoublyNode<T> next = null;
+        DoublyNode<T> previous = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            current.prev = next;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
