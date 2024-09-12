dab3893e8c9b0cc11fada6eae67a4556866606b9
Pasha <PASMO4A@verizon.com>
2024-09-12 12:17:19 +0530

    [MS12092024DS] - Added few Binary tree problems

---
+++ src/main/java/com/ms/data/structures/trees/bst/InsertNodeBST.java
@@ -0,0 +1,45 @@
+package com.ms.data.structures.trees.binarySearchTree;
+
+import com.ms.data.structures.trees.binaryTrees.TreeNode;
+
+public class InsertNodeBST {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
+    public TreeNode insertIntoBSTO(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        TreeNode curr = root;
+        while (true) {
+            if (curr.val > val) {
+                if (curr.left != null) {
+                    curr = curr.left;
+                } else {
+                    curr.left = new TreeNode(val);
+                    break;
+                }
+            } else {
+                if (curr.right != null) {
+                    curr = curr.right;
+                } else {
+                    curr.right = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
